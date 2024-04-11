be9afc32b41936ea77f4c52a070e1a55011dadfe
hadieb5 <hadiabraheem17@gmail.com>
2024-04-11 18:58:07 +0300

    mushrom

---
+++ school project/Assets/mushCount.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class mushCount : MonoBehaviour
+{
+    public TextMeshProUGUI textMeshPro;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int x = FindAnyObjectByType<mushTake>().mushNum;
+        textMeshPro.text = x.ToString();
+    }
+}
---
+++ school project/Assets/mushTake.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mushTake : MonoBehaviour
+{
+    public GameObject pickUp;
+    public GameObject mush = null;
+    public bool canTakeMushroom = false;
+    public bool desF;
+    public bool readyPickup;
+    public int mushNum = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()   
+    {
+        if (canTakeMushroom )//&& this.transform.forward == mush.position
+        {
+            pickUp.SetActive( true );
+            
+
+            if( Input.GetKey(KeyCode.F) && readyPickup) 
+            {
+                Destroy(mush);
+                mushNum ++;
+                readyPickup = false;
+            }
+            canTakeMushroom=false;
+        }
+        if(desF) 
+        {
+           pickUp.SetActive(false);
+                
+            desF=false;
+        }
+    }
+    public void OnTriggerStay(Collider other) 
+    {
+        if(other.gameObject.tag == "MUSHROOM")
+        {
+            canTakeMushroom = true;
+            mush = other.gameObject;
+            readyPickup = true;
+        }
+    }
+    
+    public void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.tag == "MUSHROOM")
+        {
+            desF = true;
+             readyPickup = false;
+        }
+    }
+}
