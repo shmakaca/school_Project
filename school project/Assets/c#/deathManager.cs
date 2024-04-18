6c3031b361b2df808582463da2fc355a96cc0120
hadieb5 <hadiabraheem17@gmail.com>
2024-04-18 22:39:22 +0300

    gg

--- school project/Assets/c#/deathManager.cs
+++ school project/Assets/c#/deathManager.cs
@@ -7,15 +7,21 @@ public class deathManager : MonoBehaviour
     public Transform resPos;
     public bool ifDead = false;
 
+
+    
+
     // Start is called before the first frame update
     void Start()
     {
         
+
     }
+
 
     // Update is called once per frame
     void Update()
     {
+        
         
         if (ifDead)
         {
--- school project/Assets/c#/fireCircle.cs
+++ school project/Assets/c#/fireCircle.cs
@@ -1,9 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class fireCircle : MonoBehaviour
 {
+    public Transform player;
+    public float radius;
+    public GameObject warning;
+    public ParticleSystem fcParticles;
+    public bool attacking = false;
+    private bool haveExit = false;
+    private bool haveEnter = false;
+    float cooldown = 2;
+    public float staticCD ;
+    public float warningTime;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +24,72 @@ public class fireCircle : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (haveExit)
+        {
+            haveEnter = false ;
+            haveExit = false ;
+        }
+
+        if(haveEnter)
+        {
+            
+
+             if (cooldown > 0 )
+             {
+                 cooldown = cooldown - Time.deltaTime;
+               
+             }
+             else
+             {
+                  attacking = true;
+                  
+                cooldown = staticCD;
+                haveEnter = false;
+             }
+              
+             
+        }
         
+
+        if (attacking)
+        {
+            warning.SetActive(true);
+            
+            Invoke("FireCircle", warningTime);
+            
+            attacking = false;
+        }
     }
+
+    void FireCircle()
+    {
+        //particles
+        fcParticles.Play();
+
+
+        //if player inside the circle player dies
+        if(Mathf.Abs(player.position.x - transform.position.x) < radius && Mathf.Abs(player.position.y - transform.position.y) < radius)
+        {
+            FindAnyObjectByType<deathManager>().ifDead = true;  
+
+        } 
+        warning?.SetActive(false);
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        
+        if (other.gameObject.tag == "Player")
+        {
+            haveEnter = true;
+            
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            haveExit = true;
+        }
+    }
+
 }
